fb7e6082a06355f29bb3b91fc2841b59a082f9d7
harshithakishor <harshitha.kotlaa@gmail.com>
2021-07-24 10:40:39 +0000

    test

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return jsonify({'hello123': 'world'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
